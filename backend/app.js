51f7726830f664a7124c5abfc515f5965e22159d
dhairay-thakur <56518563+dhairay-thakur@users.noreply.github.com>
2021-03-16 22:52:19 +0530

    add file upload

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,17 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const listingRoutes = require("./routes/listing-routes");
-const usersRoutes = require('./routes/users-routes');
+const usersRoutes = require("./routes/users-routes");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/listings", listingRoutes);
-app.use('/api/users', usersRoutes);
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Could not find this route");
@@ -18,6 +19,9 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => console.log(err));
+  }
   if (res.headerSent) {
     return next(error);
   }
--- backend/routes/listing-routes.js
+++ backend/routes/listing-routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const listingsControllers = require("../controllers/listings-controllers");
-
+const fileUpload = require("../middleware/file-upload");
 const router = express.Router();
 
 router.get("/", listingsControllers.getListings);
@@ -11,6 +11,7 @@ router.get("/:lid", listingsControllers.getListingById);
 router.get("/user/:uid", listingsControllers.getListingsByUserId);
 router.post(
   "/",
+  fileUpload.single("image"),
   [
     check("title").not().isEmpty(),
     check("price").not().isEmpty(),
